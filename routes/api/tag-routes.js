349fe47d4852cb3a983581c807be8f59d4573aa3
Aboubacar7 <abdrago17@yahoo.com>
2023-04-02 02:31:25 -0700

    adding products, tag, category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -28,21 +28,39 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new tag
 try{
-  const TagData = await Tag.create({
-    product_id: req.body.product_id,
-  });
+  const TagData = await Tag.create(req.body);
   res.status(200).json(TagData);
 }catch (err) {
   res.status(500).json(TagData);
 }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const TagData = await Tag.update( req.body, {
+      where:{
+       id: req.params.id,
+      } 
+    })
+    res.status(200).json(TagData);
+  } catch (err){
+  res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const TagData = await Tag.destroy({
+      where: {
+        id: req.params.id,
+      },
+    })
+    res.status(200).json(TagData);
+  } catch (err){
+  res.status(500).json(err);
+  }
 });
 
 module.exports = router;
